f4c49a7401319b15b5f5de33c056106ec4dd15e5
Florian de Miramon <florian@idm.io>
2024-10-10 12:51:34 +0200

    Misc

--- script/Constants/LocalConstants.sol
+++ script/Constants/LocalConstants.sol
@@ -7,48 +7,16 @@ import {MockedERC20} from "../../src/Mock/MockedERC20.sol";
 contract LocalConstants is BaseConstants {
     function construct() public {
         _markets.push(
-            Market(
-                (
-                    address(
-                        new MockedERC20("DAI", "DAI", 18, 100e18, msg.sender)
-                    )
-                ),
-                address(0),
-                300000000000000000
-            )
+            Market((address(new MockedERC20("DAI", "DAI", 18, 100e18, msg.sender))), address(0), 300000000000000000)
         );
         _markets.push(
-            Market(
-                (
-                    address(
-                        new MockedERC20("USDC", "USDC", 6, 100e18, msg.sender)
-                    )
-                ),
-                address(0),
-                300000000000000000
-            )
+            Market((address(new MockedERC20("USDC", "USDC", 6, 100e18, msg.sender))), address(0), 300000000000000000)
         );
         _markets.push(
-            Market(
-                (
-                    address(
-                        new MockedERC20("USDT", "USDT", 6, 100e18, msg.sender)
-                    )
-                ),
-                address(0),
-                300000000000000000
-            )
+            Market((address(new MockedERC20("USDT", "USDT", 6, 100e18, msg.sender))), address(0), 300000000000000000)
         );
         _markets.push(
-            Market(
-                (
-                    address(
-                        new MockedERC20("WBTC", "WBTC", 18, 100e18, msg.sender)
-                    )
-                ),
-                address(0),
-                300000000000000000
-            )
+            Market((address(new MockedERC20("WBTC", "WBTC", 18, 100e18, msg.sender))), address(0), 300000000000000000)
         );
     }
 }
--- src/Mock/MockedERC20.sol
+++ src/Mock/MockedERC20.sol
@@ -6,13 +6,9 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MockedERC20 is ERC20 {
     uint8 public customeDecimals;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimals,
-        uint256 _initialSupply,
-        address _initiator
-    ) ERC20(_name, _symbol) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply, address _initiator)
+        ERC20(_name, _symbol)
+    {
         customeDecimals = _decimals;
         _mint(_initiator, _initialSupply);
     }
