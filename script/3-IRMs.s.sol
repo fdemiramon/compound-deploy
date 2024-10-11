81630b179c635409ae6ab6b0b9a2eb93f08a152b
Adrien LF <alf@massa.net>
2024-10-11 15:04:50 +0200

    add irm

--- script/3-IRMs.s.sol
+++ script/3-IRMs.s.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.13;
 
 import {BaseScript} from "./BaseScript.sol";
+import {InterestRateModelHarness as InterestRateModel} from "../src/InterestModelFixedHarness.sol";
 
 contract IRMsScript is BaseScript {
+    InterestRateModel public interestRateModel;
+
     function run() public {
         vm.startBroadcast();
+        addAddress("InterestRateModel", address(new InterestRateModel(0)));
         vm.stopBroadcast();
     }
 }
