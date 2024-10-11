d3c21c10c046552941e74c392eacfe368b3b7e6a
Florian de Miramon <florian@idm.io>
2024-10-11 15:12:07 +0200

    Misc

--- script/Constants/LocalConstants.sol
+++ script/Constants/LocalConstants.sol
@@ -7,16 +7,44 @@ import {Script, console} from "forge-std/Script.sol";
 contract LocalConstants is BaseConstants {
     constructor() {
         _markets.push(
-            Market(address(0), "DAI", "DAI", 18, "IRM-ABC", 300000000000000000)
+            Market(
+                address(0),
+                "DAI",
+                "DAI",
+                18,
+                "IRM-Harness",
+                300000000000000000
+            )
         );
         _markets.push(
-            Market(address(0), "USDC", "USDC", 6, "IRM-ABC", 300000000000000000)
+            Market(
+                address(0),
+                "USDC",
+                "USDC",
+                6,
+                "IRM-Harness",
+                300000000000000000
+            )
         );
         _markets.push(
-            Market(address(0), "USDT", "USDT", 6, "IRM-ABC", 300000000000000000)
+            Market(
+                address(0),
+                "USDT",
+                "USDT",
+                6,
+                "IRM-Harness",
+                300000000000000000
+            )
         );
         _markets.push(
-            Market(address(0), "WBTC", "WBTC", 6, "IRM-DEF", 300000000000000000)
+            Market(
+                address(0),
+                "WBTC",
+                "WBTC",
+                6,
+                "IRM-Harness",
+                300000000000000000
+            )
         );
     }
 }
